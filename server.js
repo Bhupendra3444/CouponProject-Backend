7acddc3e7618ce077c99cffc06258b4e956b2243
bhupendra <upadhyaybhupendra24@gmail.com>
2025-03-15 21:43:44 +0530

    fixed import error

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-import { Redis } from '@upstash/redis'
+const { Redis } = require('@upstash/redis');
 const app = express();
 
 // Redis setup
